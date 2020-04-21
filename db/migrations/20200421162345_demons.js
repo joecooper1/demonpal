55b1da2e5d33b67420fcb318b4ca8ff12de68e19
joecooper1 <joecooper2991@gmail.com>
2020-04-21 16:39:24 +0100

    change to demon table

--- db/migrations/20200421162345_demons.js
+++ db/migrations/20200421162345_demons.js
@@ -1,9 +1,11 @@
 exports.up = function (knex) {
   return knex.schema.createTable("demons", (demonsTable) => {
     demonsTable.increments("demon_id").primary();
-    demonsTable.string("demonname");
+    demonsTable.string("name");
     demonsTable.string("owner").references(users.username);
-    demonsTable.timestamp("demon_created_at").defaultTo(knex.fn.now());
+    demonsTable
+      .timestamp("demon_created_at")
+      .defaultTo(knex.fn.now().slice(0, 10));
   });
 };
 
