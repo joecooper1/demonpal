be827b14ba100323c3ca80ee0d5237de9208a387
joecooper1 <joecooper2991@gmail.com>
2020-04-21 15:39:03 +0100

    setup users model

---
+++ models/users-model.js
@@ -0,0 +1,13 @@
+const connection = require("../db/connection");
+
+const selectUsers = (username = "%") => {
+  return connection("users")
+    .select("*")
+    .where("username", "like", username)
+    .then((users) => {
+      console.log(users);
+      if (users.length === 0)
+        return Promise.reject({ status: 404, msg: "Not found" });
+      else return users;
+    });
+};
