804ba55160509808f61b453c1601b503bea7c6f1
joecooper1 <joecooper2991@gmail.com>
2020-04-21 16:06:25 +0100

    new migration file

--- db/migrations/20200421160217_users.js
+++ db/migrations/20200421160217_users.js
@@ -1,6 +1,5 @@
 exports.up = function (knex) {
   return knex.schema.createTable("users", (usersTable) => {
-    usersTable.increments("user_id").unique();
     usersTable.string("username").unique().primary();
   });
 };
